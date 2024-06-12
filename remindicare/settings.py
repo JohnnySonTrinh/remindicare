44bb04f0d82644415e024d9164eb5f214844470c
John Traas <j.j.h.traas@gmail.com>
2024-06-12 15:38:29 +0200

    fix(templates): correct folder names

--- remindicare/settings.py
+++ remindicare/settings.py
@@ -61,7 +61,10 @@ ROOT_URLCONF = 'remindicare.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [
+            TEMPLATES_DIR,
+            os.path.join(BASE_DIR, 'templates'),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
