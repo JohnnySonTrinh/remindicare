1ca9133625c3391c5678d3792cd8d57a3f4a1d78
John Traas <j.j.h.traas@gmail.com>
2024-06-13 12:36:34 +0200

    feat(profiles): create database model and migrations for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Represents a user profile, extending the base User model with additional information.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_caregiver = models.BooleanField(default=False)
+    date_of_birth = models.DateField(null=True, blank=True)
+    contact_information = models.TextField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username if self.user else ''
+
+
+
