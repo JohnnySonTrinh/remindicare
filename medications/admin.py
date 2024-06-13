6d2001b41c2c89489421ec403778dc826cc39f42
John Traas <j.j.h.traas@gmail.com>
2024-06-13 23:14:59 +0200

    feat(database): update admin models

--- medications/admin.py
+++ medications/admin.py
@@ -1,42 +1,48 @@
-# from django.contrib import admin
-# from .models import UserPrescription, DoseLog, IntakeSchedule, IntakeTime, Notification, Day
+from django.contrib import admin
+from .models import UserPrescription, DoseLog, IntakeSchedule, IntakeTime, Notification, Day
 
-# class UserPrescriptionAdmin(admin.ModelAdmin):
-#     list_display = ['profile', 'product_name', 'product_type', 'dosage_amount', 'dosage_unit']
-#     search_fields = ['profile__user__username', 'product_name', 'product_type']
-#     list_filter = ['product_type', 'dosage_unit']
+class UserPrescriptionAdmin(admin.ModelAdmin):
+    list_display = ['profile', 'product_type', 'product_name', 'dosage_amount', 'dosage_unit', 'start_date', 'end_date', 'is_on_going']
+    search_fields = ['profile__user__username', 'product_name']
+    list_filter = ['product_type', 'dosage_unit', 'is_on_going']
 
-# class DoseLogAdmin(admin.ModelAdmin):
-#     list_display = ['userprescription', 'intake_time', 'intake_date', 'taken_at']
-#     search_fields = ['userprescription__profile__user__username', 'intake_time']
-#     list_filter = ['intake_date']
+class DoseLogAdmin(admin.ModelAdmin):
+    list_display = ['notification', 'user', 'taken_at', 'status', 'created_at']
+    search_fields = ['notification__intake_schedule__userprescription__profile__user__username', 'taken_at', 'status']
+    list_filter = ['status']
 
-# class DayAdmin(admin.ModelAdmin):
-#     list_display = ['name', 'times_list']
+class DayAdmin(admin.ModelAdmin):
+    list_display = ['name', 'times_list']
 
-#     def times_list(self, obj):
-#         return ', '.join([str(time) for time in obj.times.all()])
-#     times_list.short_description = 'Times'
-# class IntakeScheduleAdmin(admin.ModelAdmin):
-#     list_display = ['userprescription', 'days']
-#     search_fields = ['userprescription__profile__user__username', 'days']
-#     list_filter = ['days']
+    def times_list(self, obj):
+        return ', '.join([str(time) for time in obj.times.all()])
+    times_list.short_description = 'Times'
 
+class IntakeScheduleAdmin(admin.ModelAdmin):
+    list_display = ['userprescription', 'days_list', 'times_list']
 
-# class IntakeTimeAdmin(admin.ModelAdmin):
-#     list_display = ['time']
-#     search_fields = ['time']
+    def days_list(self, obj):
+        return ', '.join([str(day) for day in obj.days.all()])
+    days_list.short_description = 'Days'
 
-# class NotificationAdmin(admin.ModelAdmin):
-#     list_display = ['intake_schedule', 'scheduled_time', 'day', 'sent_at', 'status', 'type', 'created_at']
-#     search_fields = ['intake_schedule__userprescription__profile__user__username', 'scheduled_time', 'day']
-#     list_filter = ['status', 'type']
+    def times_list(self, obj):
+        return ', '.join([str(time) for time in obj.times.all()])
+    times_list.short_description = 'Times'
 
 
+class IntakeTimeAdmin(admin.ModelAdmin):
+    list_display = ['time']
+    search_fields = ['time']
 
-# admin.site.register(UserPrescription, UserPrescriptionAdmin)
-# admin.site.register(DoseLog, DoseLogAdmin)
-# admin.site.register(IntakeSchedule, IntakeScheduleAdmin)
-# admin.site.register(IntakeTime, IntakeTimeAdmin)
-# admin.site.register(Day, DayAdmin)
-# admin.site.register(Notification, NotificationAdmin)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['intake_schedule', 'scheduled_time', 'day', 'status']
+    search_fields = ['intake_schedule__userprescription__profile__user__username', 'scheduled_time', 'day']
+    list_filter = ['status']
+
+
+admin.site.register(UserPrescription, UserPrescriptionAdmin)
+admin.site.register(DoseLog, DoseLogAdmin)
+admin.site.register(IntakeSchedule, IntakeScheduleAdmin)
+admin.site.register(IntakeTime, IntakeTimeAdmin)
+admin.site.register(Day, DayAdmin)
+admin.site.register(Notification, NotificationAdmin)
