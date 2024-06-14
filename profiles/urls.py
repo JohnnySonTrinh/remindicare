1ef6c3e73d4dc9213ac443f8d933405f2c0b4610
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-06-14 14:11:19 +0200

    feat(views): Add url like to views

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.user_profile, name='profiles'),
+    path('<slug:pk>/', views.Profiles.as_view, name='profiles'),   
     path('caretaker/', views.caregiver_user, name='caretaker_user'),
 ]
