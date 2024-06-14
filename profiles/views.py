40e38d2301557a59a498cc6c6ab82c3ddce8c076
Ko11e <156199094+Ko11e@users.noreply.github.com>
2024-06-14 14:08:05 +0200

    feat(views): Create view for the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import Profile
+from medications.models import UserPrescription 
 
 # Create your views here.
-def user_profile(request):
-    return render(request, 'profiles/user_site.html')
+class Profiles(TemplateView):
+    """User Profile Page"""
 
+    template_name = "profiles/user_site.html"    
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(user=self.kwargs["pk"])
+        medications = UserPrescription.objects.filter(profile=profile)
+        context = {
+             "profile": profile,
+             "prescriptions": medications,
+         }
+
+        return context
+    
+    
 def caregiver_user(request):
     return render(request, 'profiles/caretaker_user.html')
