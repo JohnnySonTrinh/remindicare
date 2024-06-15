038eb8e1a08e08547560c03f2e283caeb7c0e03f
John Traas <johntraas@lime-n4qd4x9f7t.localdomain>
2024-06-15 11:16:28 +0200

    feat(schedules): user can create a schedule

--- medications/urls.py
+++ medications/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.prescription_list, name='prescription_list'),
     path('add_prescription/', views.add_prescription, name='add_prescription'),
     path('add_medication/', views.add_medication, name='add_medication'),
+    path('add_intake_schedule/<int:prescription_id>/<int:medication_id>/', views.add_intake_schedule, name='add_intake_schedule'),
+    path('schedule_list/', views.schedule_list, name='schedule_list'),
 ]
